a9486517906fe28088977059ff29d2c81c5370be
HomerOts <torresmesurhomero@gmail.com>
2023-02-01 22:31:57 -0300

    id autoincremental

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+let products = [];
+
 class ProductManager {
   constructor(title,description,price,thumbnail,code,stock,id){
     this.title = title ;
@@ -7,27 +9,48 @@ class ProductManager {
     this.thumbnail = thumbnail ;
     this.code = code ;
     this.stock = stock ;
-    this.id = id;
+    this.id = ProductManager.incrementId();
+  }
+ 
+ static incrementId() {
+  if (!this.latestId) this.latestId = 1;
+  else this.latestId++ 
+ return this.latestId
  }
  
 addProduct() {
-    products.push(product1,product2);
-  }
+      products.push(product1,product2,product3);
+    }
+
+    
+
+  
   getProducts() {
     console.log(products);
     }
-}
 
+    getProductById(id) {
+      if(this.ProductManager.some(ProductManager => ProductManager.id === id)) {
+        let producto_buscado = this.ProductManager.filter( ProductManager => ProductManager.id === id)
+        console.log(producto_buscado);
+      } else {
+        console.log("El producto no existe");
+      }
+    }
 
-const product1 = new ProductManager ('Papas fritas','Papas fritas Lays corte tradicional', 200, 'Sin imagen','lays1',10);
-const product2 = new ProductManager ('Papas fritas Jamón Serrano','Papas fritas Lays sabor Jamón Serrano', 250, 'Sin imagen','lays2',25);
+  }
 
-let products = [];
+const product1 = new ProductManager ('Citroen c4 Lounge','Citroen c4 Lounge usado', 3000000, 'Sin imagen','c4Lounge',1,);
+const product2 = new ProductManager ('Citroen Nuevo c3','Nuevo c3 First edition', 2500000, 'Sin imagen','newc3',3,);
+const product3 = new ProductManager ('Citroen Jumper','Camioneta Citroen Jumper', 5000000, 'Sin imagen','jumper',2,);
+
 
 
 product1.addProduct();
 product2.addProduct();
-product2.getProducts();
+product3.addProduct();
+console.log(products);
+product1.getProductById(1)
 
 
 
