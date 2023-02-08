10fcf10c480cd265741066a845b5db0d83c1f31e
HomerOts <torresmesurhomero@gmail.com>
2023-02-07 23:40:37 -0300

    desafio 3

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ class ProductManager {
     this.code = code ;
     this.stock = stock ;
     this.id = ProductManager.incrementId();
-    this.path = path;
+    this.path = './products.json';
   }
  
  static incrementId() {
@@ -23,8 +23,14 @@ addProduct() {
       products.push(product1,product2,product3);
     }
   
-  getProducts() {
-    console.log(products);
+  getProducts() { 
+    let products = fs.readFileSync(this.path)
+    products = JSON.parse(products)
+    if(products){
+      return products
+    } else {
+      console.log("Not products yet")
+    }
     }
 
 
@@ -36,9 +42,20 @@ addProduct() {
       } 
     
     }  */
+
+    getProductById(id) {
+      let buscarProductos = products.find(products => products.id === id);
+      if (!buscarProductos) {
+        console.log ("El producto no existe")
+      } else{
+        console.log(buscarProductos);
+      }
+       
+    }
     
 
     updateProduct() {
+    
 
     }
 
@@ -48,21 +65,46 @@ addProduct() {
 
   }
 
-const product1 = new ProductManager ('Citroen c4 Lounge','Citroen c4 Lounge usado', 3000000, 'Sin imagen','c4Lounge',1,);
-const product2 = new ProductManager ('Citroen Nuevo c3','Nuevo c3 First edition', 2500000, 'Sin imagen','newc3',3,);
-const product3 = new ProductManager ('Citroen Jumper','Camioneta Citroen Jumper', 5000000, 'Sin imagen','jumper',2,);
-
+const product1 = new ProductManager ('Citroen c4 Lounge','Citroen c4 Lounge usado', 3000000, 'Sin imagen','c4Lounge',1,'./products.json');
+const product2 = new ProductManager ('Citroen Nuevo c3','Nuevo c3 First edition', 2500000, 'Sin imagen','newc3',3,'./products.json');
+const product3 = new ProductManager ('Citroen Jumper','Camioneta Citroen Jumper', 5000000, 'Sin imagen','jumper',2,'./products.json');
 
 
 product1.addProduct();
-product2.addProduct();
-product3.addProduct();
 console.log(products);
-// getProductById(3);
+
+//getProductById(3);
+//updateProduct();
+//getProducts(); No logro llamar a esta funcion sin error, ni lograr q los productos pasen al json
+
+export default new ProductManager("./products.json");
 
 
 
+import express, { json, response } from "express"
+
+const app = express();
+ 
+const productos = products;
+
+app.get('/getProducts', (req, res) => {
+    res.send(productos) 
+})
+
+app.use(express.urlencoded({extended:true}));
 
 
+app.get('/',(req,res) => {
+let limit = req.query.limit;
+if (!limit||(limit!==0)) return res.send({products})
+let productosFiltrados = products.filter(products=> products.limit === limit);
+res.send({products:productosFiltrados})
+})
 
+//Config del servidor
+const PORT = 8080
+const server = app.listen(PORT,() => {
+    console.log('servidor ejecutandose en el puerto: ', PORT)
+})
 
+server.on('error', error => console.log('Error en el servidor: ', error));
